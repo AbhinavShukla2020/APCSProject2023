978f2a74277a753f8d590180ee2bc1aac26f899b
AbhinavShukla2020 <abhinavshukla408@gmail.com>
2023-10-17 13:26:58 -0700

    First

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,31 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class Game extends PApplet {
     // TODO: declare game variables
+    Player player;
+    //Shop shop;
+    //    restaurant;
+    // ArrayList<Bullet> bullets;
+    ArrayList<Monster> enemies;
 
     public void settings() {
         size(800, 800);   // set the window size
-
     }
 
     public void setup() {
-        // TODO: initialize game variables
+//        shop= new Shop(100,100);
+//        restaurant=new Restaurant(500,100);
+        player= new Player(50,width/2);
+        enemies = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int x=(int) (Math.random()*800);
+            int y=(int) (Math.random()*800);
+            Monster a = new Monster(x,y);
+            enemies.add(a);
+        }
+
     }
 
     /***
@@ -17,11 +33,47 @@ public class Game extends PApplet {
      * tick each object (have it update itself), and draw each object
      */
     public void draw() {
-        background(255);    // paint screen white
-        fill(0,255,0);          // load green paint color
-        ellipse(mouseX, mouseY, 60, 60);  // draw circle at mouse loc
-        ellipse(mouseX - 80, mouseY, 60, 60);  // draw circle at mouse loc
-        ellipse(mouseX + 80, mouseY, 60, 60);  // draw circle at mouse loc
+        background(200);
+        // shop.display();
+        // restaurant.display();
+        player.display(this);
+        for(Monster m : enemies){
+            m.display(this);
+        }
+        //  for(Bullet b : bullets){
+        //      b.display(this);
+        // }
+        move();
+
+    }
+
+    public void move() {
+        for(Monster m : enemies){
+            m.move();
+        }
+        if (keyPressed) {
+            player.move(key);
+        }
+    }
+
+    public void collision(){
+      /*/  if(shop.collides(player)){
+            shop.updatePlayer(player);
+        }
+        if(restaurant.collides(player)){
+            restaurant.updatePlayer(player);
+        }
+        for(Bullet b : bullets){
+            for(Monster m : enemies){
+                if(b.collides(m)){
+                    bullets.remove(b);
+                    enemies.remove(m);
+                }
+            }
+        }
+
+
+/*/
     }
 
     public static void main(String[] args) {
